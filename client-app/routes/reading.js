4a7c480dde66020b2719de63e07f4000da98d1f5
Saleh <smalhar@deakin.edu.au>
2021-08-22 15:29:54 +1000

    Filter readings by location

--- client-app/routes/reading.js
+++ client-app/routes/reading.js
@@ -11,16 +11,37 @@ var url = 'mongodb://admin:admin@localhost:27017/wqmdb'
 router.get('/', function(req, res, next) {
 
   //res.render('index', { title: 'Express' });
-  MongoClient.connect(url, function(err, client) { 
-    const db = client.db("wqmdb");    
-    var collection = db.collection('qreadings');
-    collection.find().sort({"location":1}).toArray(function (err, readings) {
-         if (err) throw err;
-         console.log(readings)
-         res.render('reading', { data: readings, title: 'Water Quality Visulization'});
-         //res.json(docs); 
-     });    
- }); 
+  var loc_name = "";
+  if (req.query.location){
+    console.log(req.query);
+    loc_name = req.query.location;
+    MongoClient.connect(url, function(err, client) { 
+      const db = client.db("wqmdb");    
+      var collection = db.collection('qreadings');
+      collection.find({location: loc_name}).sort({"location":1}).toArray(function (err, readings) {
+           if (err) throw err;
+           //console.log(readings)
+           res.render('reading', { data: readings, title: 'Water Quality Visulization'});
+           //res.json(docs); 
+       });    
+   }); 
+  }
+  else {
+    console.log("All locations!");
+    MongoClient.connect(url, function(err, client) { 
+      const db = client.db("wqmdb");    
+      var collection = db.collection('qreadings');
+      collection.find().sort({"location":1}).toArray(function (err, readings) {
+           if (err) throw err;
+           //console.log(readings)
+           res.render('reading', { data: readings, title: 'Water Quality Visulization'});
+           //res.json(docs); 
+       });    
+   }); 
+  }
+  
+
+ 
 });
 
 module.exports = router;
